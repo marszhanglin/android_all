0ce2d10539a047509e44cdfa4b6bfa705ea89d22
marszhanglin <780965203@qq.com>
2015-10-20 16:19:58 +0800

    10201619

--- All/src/mars/all/activity/two/xiaomi/XiaomiClockActivity.java
+++ All/src/mars/all/activity/two/xiaomi/XiaomiClockActivity.java
@@ -1,14 +1,14 @@
 package mars.all.activity.two.xiaomi;
 
+import mars.all.activity.base.BaseActivity;
 import mars.all.activity.two.xiaomi.view.ClockView;
-import android.app.Activity;
 import android.os.Bundle;
 
-public class XiaomiClockActivity extends Activity {
+public class XiaomiClockActivity extends BaseActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);  
 		setContentView(new ClockView(this));
 	}
 
